99c437f5d109e68c12802b4f004aae3b342cc5fb
Jaime <jamoncayop@unal.edu.co>
2020-07-18 10:56:54 -0500

    Primer commit

---
+++ IOT/app/src/main/java/com/iot/helpluis/MainActivity.java
@@ -0,0 +1,24 @@
+package com.iot.helpluis;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MainActivity extends AppCompatActivity {
+
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference myRef = database.getReference("message");
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        myRef.setValue("Hello, World! Actualizado");
+
+
+    }
+}
